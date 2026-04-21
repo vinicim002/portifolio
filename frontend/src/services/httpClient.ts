b57990f16111124f6ff284e570ec77333cb096d6
Vinicius Silva <viniciusvilanova7@gmail.com>
2026-04-21 18:59:13 -0300

    Configurando minhas variaveis de ambiente

--- frontend/src/services/httpClient.ts
+++ frontend/src/services/httpClient.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from 'axios';
 
 const httpClient: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
