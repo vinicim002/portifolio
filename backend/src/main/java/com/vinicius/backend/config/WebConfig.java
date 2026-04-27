7fdd9948bb34ac02d0799309bcb8e69b3f10f5c8
Vinicius Silva <viniciusvilanova7@gmail.com>
2026-04-26 22:16:20 -0300

    Juntando arquivos

--- backend/src/main/java/com/vinicius/backend/config/WebConfig.java
+++ backend/src/main/java/com/vinicius/backend/config/WebConfig.java
@@ -3,22 +3,34 @@ package com.vinicius.backend.config;
 import com.vinicius.backend.interceptor.RateLimitInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    // Injeta o interceptor que criamos
     @Autowired
     private RateLimitInterceptor rateLimitInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(rateLimitInterceptor)
-                // Define em quais rotas o interceptor vai atuar
-                // /messages/** significa: /messages e qualquer coisa depois
-                // ex: /messages, /messages/1, /messages/qualquercoisa
                 .addPathPatterns("/messages/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:3001",
+                        "https://portfolio-ten-psi-qervfgk3o1.vercel.app/",
+                        "https://viniciusvilanova.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
